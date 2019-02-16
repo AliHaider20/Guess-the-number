132cda9ff2977765ab8e2025b055e82b59402e69
haider456143 <42317018+haider456143@users.noreply.github.com>
2019-02-16 18:05:40 +0530

    Add files via upload

---
+++ guessing a number game.py
@@ -0,0 +1,19 @@
+
+import random
+n=random.randint(0,9)
+guess=int(input("enter a number between 0 to 9:"))
+print("")
+def check(guess):
+    while (guess!=n):
+        if guess<n:
+            print("The number  is  low")
+            print("")
+            guess=int(input("Enter once again"))
+        elif guess>n:
+            print("The number is  high")
+            print("")
+            guess=int(input("Enter once again"))
+        
+    print("you guessed it right")
+check(guess)
+
