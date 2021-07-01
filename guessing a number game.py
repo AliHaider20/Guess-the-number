cedfeca934fbfe9ce3ba36ce89efa5d9daf4b82b
Haider Ali <haiderratlamwala786@gmail.com>
2021-07-01 12:08:04 +0530

    Feature added
    Added number of guesses left.

--- guessing a number game.py
+++ guessing a number game.py
@@ -1,19 +1,19 @@
-
 import random
 n=random.randint(0,9)
-guess=int(input("enter a number between 0 to 9:"))
-print("")
+guess=int(input("enter a number between 0 to 9:\n"))
 def check(guess):
-    while (guess!=n):
-        if guess<n:
-            print("The number  is  low")
-            print("")
-            guess=int(input("Enter once again"))
-        elif guess>n:
-            print("The number is  high")
-            print("")
-            guess=int(input("Enter once again"))
-        
-    print("you guessed it right")
-check(guess)
+   guesses = 3
+   while (guess!=n and guesses!=0):
+      if guess<n:
+         print("The number  is  low")
+         print(f"{guesses} guesses are left\n")
+         guess=int(input("Enter once again"))
+      elif guess>n:
+         print("The number is  high")
+         print(f"{guesses} guesses are left\n")
+         guess=int(input("Enter once again"))
+      guesses-=1
+   print("Hurray! You guessed it right")
 
+if __name__ == "__main__":
+   check(guess)
